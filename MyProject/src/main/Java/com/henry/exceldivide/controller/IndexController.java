fca962dd641273cbe4350c1acf27fbc5a584743d
Neverslave <1029110566@qq.com>
2019-07-10 00:01:20 +0800

    添加进度条

--- MyProject/src/main/Java/com/henry/exceldivide/controller/IndexController.java
+++ MyProject/src/main/Java/com/henry/exceldivide/controller/IndexController.java
@@ -1,9 +1,17 @@
 package com.henry.exceldivide.controller;
 
 import com.jfinal.core.Controller;
+import com.jfinal.upload.UploadFile;
+
 
 public class IndexController extends Controller {
     public void index(){
         render("/views/index.html");
+    }
+
+    public void upload(){
+        UploadFile  uploadFile = getFile();
+        System.out.println(uploadFile.getFileName());
+
     }
 }
